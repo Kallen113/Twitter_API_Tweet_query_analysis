94a92092c99ddaa3a4747ab8007f352484181f03
Kallen113 <35751364+Kallen113@users.noreply.github.com>
2019-03-01 14:56:03 -0800

    Create constants.py
    This script specifies the consumer access key, request token, access token, and authorization URL, which are all needed to access the Twitter API.

---
+++ constants.py
@@ -0,0 +1,8 @@
+#inout consumer and secret API keys from the Twitter app
+#note this link to Twitter app page for the app I'm developing: <https://developer.twitter.com/en/apps/16137719>
+CONSUMER_KEY = 'gFHAZ92UNyma9WbPDOfdlawKU'
+CONSUMER_SECRET = 'DVRFY8N7ll7u08IUDJJaJfpCcBWHUDp5KlQWXJzPDPoeuajQWb'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
