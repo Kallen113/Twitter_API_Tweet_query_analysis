e3e2188ca1543fa5233a61b1874444b9b1e0175e
Kallen113 <35751364+Kallen113@users.noreply.github.com>
2019-03-02 15:43:32 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-#inout consumer and secret API keys from the Twitter app
+#input consumer and secret API keys from the Twitter app
 #note this link to Twitter app page for the app I'm developing: <https://developer.twitter.com/en/apps/16137719>
 CONSUMER_KEY = 'gFHAZ92UNyma9WbPDOfdlawKU'
 CONSUMER_SECRET = 'DVRFY8N7ll7u08IUDJJaJfpCcBWHUDp5KlQWXJzPDPoeuajQWb'
