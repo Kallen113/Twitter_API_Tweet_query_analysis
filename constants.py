7046204c682d5a608b2c36b30dc1de534483986f
Kallen113 <35751364+Kallen113@users.noreply.github.com>
2021-07-20 02:21:32 -0700

    Updated constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #input consumer and secret API keys from the Twitter app
 #note this link to Twitter app page for the app I'm developing: <https://developer.twitter.com/en/apps/16137719>
-CONSUMER_KEY = 'gFHAZ92UNyma9WbPDOfdlawKU'
-CONSUMER_SECRET = 'DVRFY8N7ll7u08IUDJJaJfpCcBWHUDp5KlQWXJzPDPoeuajQWb'
+CONSUMER_KEY = 'xnuz123'
+CONSUMER_SECRET = 'adfkwhat555'
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
